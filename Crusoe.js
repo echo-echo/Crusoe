c145f71c2664f8039e10bc48183137beaf2be3bf
Josh Benson <bensonjosha@gmail.com>
2015-04-30 16:00:20 -0500

    adds mapboxjs map to page, nonfunctioning message box

--- Crusoe.js
+++ Crusoe.js
@@ -1,5 +1,19 @@
 if (Meteor.isClient) {
   // counter starts at 0
+
+$(document).ready(function(){
+
+  L.mapbox.accessToken = 'pk.eyJ1Ijoiam9zaHVhYmVuc29uIiwiYSI6Im1sT3BqRWcifQ.W7h8nMmj_oI1p4RzChElsQ';
+  // Replace 'examples.map-i87786ca' with your map id.
+  var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v4/joshuabenson.68d254d5/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
+    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+  });
+
+  var map = L.map('map')
+  .addLayer(mapboxTiles)
+  .setView([42.3610, -71.0587], 15);
+});
+
   Session.setDefault('counter', 0);
 
   Template.hello.helpers({
@@ -14,6 +28,7 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
 
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
