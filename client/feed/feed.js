27b04b5f36597904642d887e4bb957f2d4291208
Benita <benitazhang@gmail.com>
2015-05-01 17:06:43 -0500

    modal

--- client/feed/feed.js
+++ client/feed/feed.js
@@ -3,16 +3,43 @@ Messages = new Mongo.Collection("messages")
 Meteor.subscribe("messages");
 
 Template.feed.helpers({
-	showBox: function(){
-		return Session.get("show");
-	},
+	// showBox: function(){
+	// 	return Session.get("show");
+	// },
 	messages: function(){
 		return Messages.find({})
 	}
 })
 
 Template.feed.events({
-	"submit .compose": function(event){
+	// "submit .compose": function(event){
+	// 	var text = event.target.text.value;
+		
+	// 	Msg.insert({
+	// 		text:text,
+	// 		createdAt: new Date(),
+	// 		username: Meteor.user().username,
+	// 		// //test data for map view to use
+	// 		location: {
+	// 				"type": "Point",
+	// 				"coordinates": [-97.75, 30.25]
+	// 			}
+	// 	})
+
+	// 	event.target.text.value=""
+	// 	return false;
+	// }
+
+	"click .btn": function(){
+		// var showHide = !Session.get("show")
+		// Session.set("show", showHide);
+		AntiModals.overlay('writeModal')
+
+	}
+})
+
+Template.writeModal.events({
+		"submit .compose": function(event){
 		var text = event.target.text.value;
     // test data
     var location = {
@@ -24,10 +51,5 @@ Template.feed.events({
 
 		event.target.text.value=""
 		return false;
-	},
-
-	"click .btn": function(){
-		var showHide = !Session.get("show")
-		Session.set("show", showHide);
 	}
 })
