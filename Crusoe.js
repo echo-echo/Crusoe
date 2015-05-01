f45febd632c2fc5589e2e130482f4e50092205ba
Josh Benson <bensonjosha@gmail.com>
2015-04-30 19:42:34 -0500

    button will bring to current location

--- Crusoe.js
+++ Crusoe.js
@@ -3,15 +3,61 @@ if (Meteor.isClient) {
 
 $(document).ready(function(){
 
-  L.mapbox.accessToken = 'pk.eyJ1Ijoiam9zaHVhYmVuc29uIiwiYSI6Im1sT3BqRWcifQ.W7h8nMmj_oI1p4RzChElsQ';
-  // Replace 'examples.map-i87786ca' with your map id.
-  var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v4/joshuabenson.68d254d5/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
-    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
-  });
+  // L.mapbox.accessToken = 'pk.eyJ1Ijoiam9zaHVhYmVuc29uIiwiYSI6Im1sT3BqRWcifQ.W7h8nMmj_oI1p4RzChElsQ';
+  // // Replace 'examples.map-i87786ca' with your map id.
+  // var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v4/joshuabenson.68d254d5/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
+  //   attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+  // });
 
-  var map = L.map('map')
-  .addLayer(mapboxTiles)
-  .setView([42.3610, -71.0587], 15);
+ L.mapbox.accessToken = 'pk.eyJ1Ijoiam9zaHVhYmVuc29uIiwiYSI6Im1sT3BqRWcifQ.W7h8nMmj_oI1p4RzChElsQ';
+var geolocate = document.getElementById('geolocate');
+var map = L.mapbox.map('map', 'examples.map-i86nkdio');
+
+var myLayer = L.mapbox.featureLayer().addTo(map);
+
+// This uses the HTML5 geolocation API, which is available on
+// most mobile browsers and modern browsers, but not in Internet Explorer
+//
+// See this chart of compatibility for details:
+// http://caniuse.com/#feat=geolocation
+if (!navigator.geolocation) {
+    geolocate.innerHTML = 'Geolocation is not available';
+} else {
+    geolocate.onclick = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        map.locate();
+    };
+}
+
+// Once we've got a position, zoom and center the map
+// on it, and add a single marker.
+map.on('locationfound', function(e) {
+    map.fitBounds(e.bounds);
+
+    myLayer.setGeoJSON({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [e.latlng.lng, e.latlng.lat]
+        },
+        properties: {
+            'title': 'Here I am!',
+            'marker-color': '#ff8888',
+            'marker-symbol': 'star'
+        }
+    });
+
+    // And hide the geolocation button
+    geolocate.parentNode.removeChild(geolocate);
+});
+
+// If the user chooses not to allow their location
+// to be shared, display an error message.
+map.on('locationerror', function() {
+    geolocate.innerHTML = 'Position could not be found';
+});
+
 });
 
   Session.setDefault('counter', 0);
