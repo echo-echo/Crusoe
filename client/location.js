3466bf987e34e7469ec43b303fb40d4a005531b5
Josh Benson <bensonjosha@gmail.com>
2015-05-02 12:46:27 -0500

    location working

--- client/location.js
+++ client/location.js
@@ -1,15 +1,5 @@
-Template.Map.rendered = function () {
-  var marker;
-  Deps.autorun(function () {
-    if (Mapbox.loaded()) {
-      Session.set("test", "loaded")
-      L.mapbox.accessToken = "pk.eyJ1Ijoiam9zaHVhYmVuc29uIiwiYSI6Im1sT3BqRWcifQ.W7h8nMmj_oI1p4RzChElsQ";
-      var map = L.mapbox.map('map', 'joshuabenson.68d254d5');
-      marker = L.marker([30.272920898023475, -97.74438629799988]).addTo(map)
-      // var featureLayer = L.mapbox.featureLayer()
-
-    }
-  });
+// Template.Map.rendered = function () {
+//   var marker;
 
   Tracker.autorun(function(){
     var local = Geolocation.currentLocation()
@@ -19,4 +9,4 @@ Template.Map.rendered = function () {
       Session.set('loc', local) 
     }
   })
-}
+// }
--- client/map/map.js
+++ client/map/map.js
@@ -4,12 +4,16 @@ Meteor.startup(function(){
 
 Meteor.subscribe("messages");
 
-Template.Map.rendered = function () {
+// Template.Map.rendered = function () {
 
-  this.autorun(function () {
+  Deps.autorun(function () {
     if (Mapbox.loaded()) {
-      L.mapbox.accessToken = 'pk.eyJ1Ijoiam9zaHVhYmVuc29uIiwiYSI6Im1sT3BqRWcifQ.W7h8nMmj_oI1p4RzChElsQ';
-      var map = L.mapbox.map('map',  'joshuabenson.68d254d5').setZoom(15);
+      Session.set("test", "loaded")
+      L.mapbox.accessToken = "pk.eyJ1Ijoiam9zaHVhYmVuc29uIiwiYSI6Im1sT3BqRWcifQ.W7h8nMmj_oI1p4RzChElsQ";
+      var map = L.mapbox.map('map', 'joshuabenson.68d254d5');
+      marker = L.marker([30.272920898023475, -97.74438629799988]).addTo(map)
+      // var featureLayer = L.mapbox.featureLayer()
+
     }
   });
-};
+// };
