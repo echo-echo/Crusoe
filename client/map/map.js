2f917ad830d0790a11acdec0de020d80fab66c99
Josh Benson <bensonjosha@gmail.com>
2015-05-02 14:35:42 -0500

    removed superfluous session set call

--- client/map/map.js
+++ client/map/map.js
@@ -8,7 +8,7 @@ Meteor.subscribe("messages");
 
   Deps.autorun(function () {
     if (Mapbox.loaded()) {
-      Session.set("test", "loaded")
+      // Session.set("test", "loaded")
       L.mapbox.accessToken = "pk.eyJ1Ijoiam9zaHVhYmVuc29uIiwiYSI6Im1sT3BqRWcifQ.W7h8nMmj_oI1p4RzChElsQ";
       var map = L.mapbox.map('map', 'joshuabenson.68d254d5');
       marker = L.marker([30.272920898023475, -97.74438629799988]).addTo(map)
