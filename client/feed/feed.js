fc01b919984aebffbeb42a50b46816fb8f6fbc66
karmakettle <karmakettle@gmail.com>
2015-05-02 15:48:46 -0500

    some progress on individual message modals

--- client/feed/feed.js
+++ client/feed/feed.js
@@ -1,4 +1,4 @@
-Messages = new Mongo.Collection("messages")
+Messages = new Mongo.Collection("messages");
 
 Meteor.subscribe("messages");
 
@@ -7,28 +7,40 @@ Template.feed.helpers({
 	// 	return Session.get("show");
 	// },
 	messages: function(){
-		return Messages.find({},{sort: {createdAt: -1}})
+		return Messages.find({},{sort: {createdAt: -1}});
 	}
 })
 
 Template.feed.events({
 	"click .btn": function(){
-		AntiModals.overlay('writeModal')
+		AntiModals.overlay('writeModal');
+	},
+	"click li": function(event){
+		var message = event.target;
+		console.log("this is event.target: ", message);
+		AntiModals.overlay('messageModal', message);
 	}
-})
+});
+
+Template.messageModal.helpers({
+	showMessage: function(message) {
+		console.log("The message made it past the event listener?: ", message);
+		return message;
+	}
+});
 
 Template.writeModal.events({
 		"submit .compose": function(event){
 		var text = event.target.text.value;
     // test data
     var location = {
-          "type": "Point",
-          "coordinates": [-97.75, 30.25]
-        }
+      "type": "Point",
+      "coordinates": [-97.75, 30.25]
+    };
 
-    Meteor.call("addMessage", text, location)
+    Meteor.call("addMessage", text, location);
 
-		event.target.text.value=""
+		event.target.text.value="";
 		return false;
 	}
-})
+});
