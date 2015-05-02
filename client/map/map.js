72034ffb71a2088e39091fda2a005767f11866a8
Jamie Skinner <skinner520@gmail.com>
2015-05-02 15:54:52 -0500

    got rid of controlls and zoom feature works

--- client/map/map.js
+++ client/map/map.js
@@ -5,15 +5,31 @@ Meteor.startup(function(){
 
 Template.Map.rendered = function () {
   Meteor.subscribe("messages");
-
+  var marker
   Deps.autorun(function () {
     if (Mapbox.loaded()) {
-      // Session.set("test", "loaded")
       L.mapbox.accessToken = "pk.eyJ1Ijoiam9zaHVhYmVuc29uIiwiYSI6Im1sT3BqRWcifQ.W7h8nMmj_oI1p4RzChElsQ";
-      var map = L.mapbox.map('map', 'joshuabenson.68d254d5');
-      marker = L.marker([30.272920898023475, -97.74438629799988]).addTo(map)
-      // var featureLayer = L.mapbox.featureLayer()
+      var map = L.mapbox.map('map', 'joshuabenson.68d254d5', { 
+        //map options
+        attributionControl: false,
+        zoomControl :false,
 
+      })
+      //default marker location
+      marker = L.marker([30.272920898023475, -97.74438629799988]).addTo(map)
+      map.panTo([30.272920898023475, -97.74438629799988])
+      map.setZoom(14)
     }
   });
+
+
+    Tracker.autorun(function(){
+    var local = Geolocation.currentLocation()
+    if(local && marker){
+      console.log(local)
+      marker.setLatLng([local.coords.latitude, local.coords.longitude]).update();
+      Session.set('loc', local) 
+      map.panTo([local.coords.latitude, local.coords.longitude])
+    }
+  })
 };
