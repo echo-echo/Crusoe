9846e088be3b90e38832aca2c02c1aff81b20332
Jamie Skinner <skinner520@gmail.com>
2015-05-02 17:34:16 -0500

    added Toggle Pan option to the map

--- client/map/map.js
+++ client/map/map.js
@@ -3,13 +3,22 @@ Meteor.startup(function(){
 });
 
 
+Meteor.subscribe("messages");
+
+Template.Map.events({
+  "click #panButton": function(){
+    console.log('clicked', Session.get('pan'))
+    Session.set('pan', !Session.get('pan'))
+  }
+})
+
 Template.Map.rendered = function () {
-  Meteor.subscribe("messages");
-  var marker
+  var marker;
+  var map;
   Deps.autorun(function () {
     if (Mapbox.loaded()) {
       L.mapbox.accessToken = "pk.eyJ1Ijoiam9zaHVhYmVuc29uIiwiYSI6Im1sT3BqRWcifQ.W7h8nMmj_oI1p4RzChElsQ";
-      var map = L.mapbox.map('map', 'joshuabenson.68d254d5', { 
+      map = L.mapbox.map('map', 'joshuabenson.68d254d5', { 
         //map options
         attributionControl: false,
         zoomControl :false,
@@ -19,17 +28,20 @@ Template.Map.rendered = function () {
       marker = L.marker([30.272920898023475, -97.74438629799988]).addTo(map)
       map.panTo([30.272920898023475, -97.74438629799988])
       map.setZoom(14)
-    }
+    } 
   });
 
-
-    Tracker.autorun(function(){
-    var local = Geolocation.currentLocation()
+  
+  Tracker.autorun(function(){
+  var local = Geolocation.currentLocation()
     if(local && marker){
       console.log(local)
       marker.setLatLng([local.coords.latitude, local.coords.longitude]).update();
       Session.set('loc', local) 
-      map.panTo([local.coords.latitude, local.coords.longitude])
-    }
+      if(Session.get('pan')){
+        map.panTo([local.coords.latitude, local.coords.longitude])
+      }
+    } 
   })
 };
+
