121bbbc88a116c0ade6d09e780f12371f7856d7f
Benita <benitazhang@gmail.com>
2015-05-01 19:42:29 -0500

    styling

--- client/feed/feed.js
+++ client/feed/feed.js
@@ -5,34 +5,13 @@ Template.feed.helpers({
 	// 	return Session.get("show");
 	// },
 	messages: function(){
-		return Msg.find({})
+		return Msg.find({},{sort: {createdAt: -1}})
 	}
 })
 
 Template.feed.events({
-	// "submit .compose": function(event){
-	// 	var text = event.target.text.value;
-		
-	// 	Msg.insert({
-	// 		text:text,
-	// 		createdAt: new Date(),
-	// 		username: Meteor.user().username,
-	// 		// //test data for map view to use
-	// 		location: {
-	// 				"type": "Point",
-	// 				"coordinates": [-97.75, 30.25]
-	// 			}
-	// 	})
-
-	// 	event.target.text.value=""
-	// 	return false;
-	// }
-
 	"click .btn": function(){
-		// var showHide = !Session.get("show")
-		// Session.set("show", showHide);
 		AntiModals.overlay('writeModal')
-
 	}
 })
 
