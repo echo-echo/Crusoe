e8f6a1ed358b97ba57e3e79eb11f08a2016cde5d
karmakettle <karmakettle@gmail.com>
2015-05-04 17:19:08 -0500

    added percolate:synced-cron pkg, scheduled job once a second in server.js

--- server/server.js
+++ server/server.js
@@ -4,3 +4,22 @@ Messages = new Mongo.Collection("messages")
 Meteor.publish("messages", function() {
   return Messages.find();
 })
+
+// percolate:synced-cron methods--adds a cron job
+SyncedCron.add({
+  name: 'Update messages worldwide',
+  // parse is a later.parse object
+  //  percolate:synced-cron is built on top of Later.js
+  schedule: function(parse) {
+    return parse.cron('* * * * * *', true);
+  },
+  job: function () {
+    // var messages = Messages.find({});
+    console.log("once a second?");
+    // for ( var i = 0; i < messages.length; i++ ) {
+    //   console.log(messages[i]);
+    // }
+  }
+});
+
+SyncedCron.start();
