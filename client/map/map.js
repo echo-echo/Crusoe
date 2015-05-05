8c444e132619f8c9dd2858458d0d0038cf15f8d3
karmakettle <karmakettle@gmail.com>
2015-05-05 15:37:29 -0500

    added custom message-bottle icons for messages in and out of user range

--- client/map/map.js
+++ client/map/map.js
@@ -39,64 +39,74 @@ Template.Map.rendered = function () {
 			var userLong = Number(localStorage.getItem("userLong"));
       var geoJsons = [];
 
-		///////////////////////////////////////////////////////////////
-		//Haversine Formula - find distance btwn two points on sphere//
-				var getProx = function(lat1,lon1,lat2,lon2) {
-			      var R = 6371;
-			      var dLat = deg2rad(lat2-lat1);
-			      var dLon = deg2rad(lon2-lon1); 
-			      var a = 
-			        Math.sin(dLat/2) * Math.sin(dLat/2) +
-			        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-			        Math.sin(dLon/2) * Math.sin(dLon/2)
-			        ; 
-			      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-			      var d = R * c; // Distance in km
-			      return d;
-			    }
+    ///////////////////////////////////////////////////////////////
+    //Haversine Formula - find distance btwn two points on sphere//
+        var getProx = function(lat1,lon1,lat2,lon2) {
+            var R = 6371;
+            var dLat = deg2rad(lat2-lat1);
+            var dLon = deg2rad(lon2-lon1); 
+            var a = 
+              Math.sin(dLat/2) * Math.sin(dLat/2) +
+              Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+              Math.sin(dLon/2) * Math.sin(dLon/2)
+              ; 
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+            var d = R * c; // Distance in km
+            return d;
+          }
 
-			    var deg2rad = function(deg) {
-			      return deg * (Math.PI/180)
-			    }
-		///////////////////////////////////////////////////////////////
-		/////filter by proximity between message and user location/////
+          var deg2rad = function(deg) {
+            return deg * (Math.PI/180)
+          }
+    ///////////////////////////////////////////////////////////////
+    /////filter by proximity between message and user location/////
 
       allMess.forEach(function(object){
-      	var msgLat = object.location.coordinates[1]
-				var msgLong = object.location.coordinates[0]
-				var proximity = getProx(msgLat,msgLong,userLat,userLong)
-				if (proximity<2){
-	        geoJsons.push({
-	          "type": "Feature",
-	          "geometry": {
-	            "type": "Point",
-	            "coordinates": [msgLong, msgLat]
-	          },
-	          "properties": {
-	            "title": object.text,
-	            "description": object.createdAt,
-	            "marker-color": "#fc4353",
-	            "marker-size": "large",
-	            "marker-symbol": "star"
-	          }
-	        });
-				}else{
-					 geoJsons.push({
-	          "type": "Feature",
-	          "geometry": {
-	            "type": "Point",
-	            "coordinates": [msgLong, msgLat]
-	          },
-	          "properties": {
-	          	"title": "too far to view message",
-	            "description": object.createdAt,
-	            "marker-color": "#80818A",
-	            "marker-size": "large",
-	            "marker-symbol": "circle"
-	          }
-	        });
-				}
-      })
+        var msgLat = object.location.coordinates[1]
+        var msgLong = object.location.coordinates[0]
+        var proximity = getProx(msgLat,msgLong,userLat,userLong)
+        if (proximity<2){
+          geoJsons.push({
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [msgLong, msgLat]
+            },
+            "properties": {
+              "title": object.text,
+              "description": object.createdAt,
+              "icon": {
+                "iconUrl": "/message.png",
+                "iconSize": [50, 50]
+              }
+            }
+          });
+        }else{
+           geoJsons.push({
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [msgLong, msgLat]
+            },
+            "properties": {
+              "title": "too far to view message",
+              "description": object.createdAt,
+              "icon": {
+                "iconUrl": "/message-off.png",
+                "iconSize": [50, 50]
+              }
+            }
+          });
+        }
+      });
+
+      map.featureLayer.on('layeradd', function (e) {
+        var marker = e.layer;
+        feature = marker.feature;
+
+        marker.setIcon(L.icon(feature.properties.icon));
+      });
+
       //add array of geoJson objects to map layer:
       map.featureLayer.setGeoJSON(geoJsons);
       //default marker location
