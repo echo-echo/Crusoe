e786fc203e85846c288a2f2cb371478d8021c5d7
Ben Johnson <ben@bjmfactory.com>
2015-05-05 16:38:30 -0500

    adds basic movement algorithm using weights

--- server/server.js
+++ server/server.js
@@ -17,8 +17,12 @@ SyncedCron.add({
     var messages = Messages.find({});
 
     messages.forEach(function (msg) {
-      var newLat = msg.location.coordinates[0] += .00001;
-      var newLng = msg.location.coordinates[1] += .00001;
+      // maybe change this to sine later to make it smoother
+      // also you can still add in lat and lng weights for 1hr, 1day, 1wk, 1month, 1year
+      var latChange = (Math.random() - 0.5 + msg.latWeight10s + msg.latWeight30s + msg.latWeight1m) * 0.00001;
+      var lngChange = (Math.random() - 0.5 + msg.lngWeight10s + msg.lngWeight30s + msg.lngWeight1m) * 0.00001;
+      var newLat = msg.location.coordinates[0] += latChange;
+      var newLng = msg.location.coordinates[1] += lngChange;
       Messages.update({_id: msg._id}, {$set: {"location.coordinates": [newLat, newLng]} });
     });
   }
