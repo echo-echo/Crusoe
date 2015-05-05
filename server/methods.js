dc0fe8ab8b8ba5700068f837eda9904be54cc801
Ben Johnson <ben@bjmfactory.com>
2015-05-05 16:37:38 -0500

    adds lat and lng weights to messages for 10s, 30s, 1m

--- server/methods.js
+++ server/methods.js
@@ -7,9 +7,15 @@ Meteor.methods({
       text: text,
       createdAt: new Date(),
       username: username,
-      location: {"type": "Point","coordinates": location}
+      location: {"type": "Point","coordinates": location},
+      latWeight10s: Math.random() - 0.5,
+      lngWeight10s: Math.random() - 0.5,
+      latWeight30s: Math.random() * (0.6) - 0.3,
+      lngWeight30s: Math.random() * (0.6) - 0.3,
+      latWeight1m: Math.random() * (0.2) - 0.1,
+      lngWeight1m: Math.random() * (0.2) - 0.1
     });
   }
 })
 
-   
+
