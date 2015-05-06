ff50d10dce58126b8b104cec44abf6312fef53f7
Ben Johnson <ben@bjmfactory.com>
2015-05-06 10:27:46 -0500

    changes weights for movement algorithm

--- server/methods.js
+++ server/methods.js
@@ -7,12 +7,16 @@ Meteor.methods({
       createdAt: new Date(),
       username: username,
       location: {"type": "Point","coordinates": location},
-      latWeight10s: Math.random() - 0.5,
-      lngWeight10s: Math.random() - 0.5,
-      latWeight30s: Math.random() * (0.6) - 0.3,
-      lngWeight30s: Math.random() * (0.6) - 0.3,
-      latWeight1m: Math.random() * (0.2) - 0.1,
-      lngWeight1m: Math.random() * (0.2) - 0.1
+      latWeight15s: Math.random() - 0.5,
+      lngWeight15s: Math.random() - 0.5,
+      latWeight1m: Math.random() - 0.5,
+      lngWeight1m: Math.random() - 0.5,
+      latWeight15m: Math.random() - 0.5,
+      lngWeight15m: Math.random() - 0.5,
+      latWeight1hr: Math.random() - 0.5,
+      lngWeight1hr: Math.random() - 0.5,
+      latWeight6hr: Math.random() - 0.5,
+      lngWeight6hr: Math.random() - 0.5,
     });
   },
 
--- server/server.js
+++ server/server.js
@@ -25,8 +25,22 @@ SyncedCron.add({
     messages.forEach(function (msg) {
       // maybe change this to sine later to make it smoother
       // also you can still add in lat and lng weights for 1hr, 1day, 1wk, 1month, 1year
-      var latChange = (Math.random() - 0.5 + msg.latWeight10s + msg.latWeight30s + msg.latWeight1m) * 0.00001;
-      var lngChange = (Math.random() - 0.5 + msg.lngWeight10s + msg.lngWeight30s + msg.lngWeight1m) * 0.00001;
+      var latChange = (
+        Math.random() - 0.5 +
+        msg.latWeight15s +
+        msg.latWeight1m +
+        msg.latWeight15m +
+        msg.latWeight1hr +
+        msg.latWeight6hr
+        ) * 0.00001;
+      var lngChange = (
+        Math.random() - 0.5 +
+        msg.lngWeight15s +
+        msg.lngWeight1m +
+        msg.lngWeight15m +
+        msg.lngWeight1hr +
+        msg.lngWeight6hr
+        ) * 0.00001;
       var newLat = msg.location.coordinates[0] += latChange;
       var newLng = msg.location.coordinates[1] += lngChange;
       Messages.update({_id: msg._id}, {$set: {"location.coordinates": [newLat, newLng]} });
