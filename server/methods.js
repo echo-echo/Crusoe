c79dcc309662779b391de7d1429e29dab24ac09d
Benita <benitazhang@gmail.com>
2015-05-05 19:55:47 -0500

    profile basics working

--- server/methods.js
+++ server/methods.js
@@ -2,16 +2,18 @@ Meteor.methods({
   addMessage: function (text, location) {
     var username = Meteor.user() ? Meteor.user().username : "Anonymous";
 
-    Messages.insert({
+      Messages.insert({
       text: text,
       createdAt: new Date(),
       username: username,
-      location: {"type": "Point","coordinates": location}
-    }, function(err,result){
-    	console.log(result)
+      location: {"type": "Point","coordinates": location},
+      latWeight10s: Math.random() - 0.5,
+      lngWeight10s: Math.random() - 0.5,
+      latWeight30s: Math.random() * (0.6) - 0.3,
+      lngWeight30s: Math.random() * (0.6) - 0.3,
+      latWeight1m: Math.random() * (0.2) - 0.1,
+      lngWeight1m: Math.random() * (0.2) - 0.1
     });
-
-
   },
 
   tagMessage: function(messageId){
