5c64f7ffb8623ea64c871183005d83d7a04a7c14
karmakettle <karmakettle@gmail.com>
2015-05-09 14:33:58 -0500

    ability to add messages

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -1,26 +1,37 @@
-Meteor.subscribe("userData")
+Meteor.subscribe("userData");
 
 Template.profile.rendered = function(){
-	$('ul.tabs').tabs()
-	$('.modal-trigger').leanModal()
+  $('ul.tabs').tabs()
+  $('.modal-trigger').leanModal()
 }
 
 Template.profile.helpers({
-	taggedMessages: function(){
-		var result=[]
-		var tagged = Meteor.users.find({}).fetch()[0].tagged
+  taggedMessages: function(){
+    var result=[]
+    var tagged = Meteor.users.find({}).fetch()[0].tagged
 
-		
-		tagged.forEach(function(item){
-			result.push(Messages.find({_id:item}).fetch()[0])
+    if ( tagged ) {
+      tagged.forEach(function(item){
+        result.push(Messages.find({_id:item}).fetch()[0]);
+      });
 
-		})
+      return result;
+    }
+  },
 
-		return result
-	},
+  userCreated: function(){
+    var created = Messages.find({username:Meteor.user().username});
+    return created;
+  }
+});
 
-	userCreated: function(){
-		var created = Messages.find({username:Meteor.user().username})
-		return created
-	}
-})
+Template.profile.events({
+  "click .submit": function () {
+    var message = $('textarea').val();
+    var longitude = Number(localStorage.getItem("userLong"));
+    var latitude = Number(localStorage.getItem("userLat"));
+    var location=[longitude,latitude];
+
+    Meteor.call("addMessage", message, location);
+  }
+});
