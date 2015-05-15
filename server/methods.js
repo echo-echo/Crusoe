cd24c1b978a4a5c483fbecd093647cad83c6e361
Josh Benson <bensonjosha@gmail.com>
2015-05-15 16:29:22 -0500

    change files to media

--- server/methods.js
+++ server/methods.js
@@ -1,5 +1,5 @@
 Meteor.methods({
-  addMessage: function (text, location, files) {
+  addMessage: function (text, location, media) {
     var username = Meteor.user() ? Meteor.user().username || Meteor.user().profile.name : "Anonymous";
 
     Meteor.http.get('http://api.tiles.mapbox.com/v4/geocode/mapbox.places/'+location[0]+','+location[1]+'.json?access_token=pk.eyJ1Ijoiam9zaHVhYmVuc29uIiwiYSI6Im1sT3BqRWcifQ.W7h8nMmj_oI1p4RzChElsQ', function (err, res) {
