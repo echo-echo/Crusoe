a40864866e24734dfb2324345276be8bd76efe83
Benita <benitazhang@gmail.com>
2015-05-15 17:09:47 -0500

    facebook config added to server

--- server/keys.js
+++ server/keys.js
@@ -1,6 +1,11 @@
 Meteor.startup(function(){
-  process.env.AWS_ACCESS_KEY_ID = ""; //add new reset key_id
-  process.env.AWS_SECRET_ACCESS_KEY = ""; //add new reset access_key
+ process.env.AWS_ACCESS_KEY_ID = "AKIAJEDHGDLW3LVT2Q3Q";
+ process.env.AWS_SECRET_ACCESS_KEY = "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";
+ process.env.FB_APP_ID = "1617736865105666";
+ process.env.FB_SECRET = "7c63e6d71f5370e7782015f573f90bf9";
+
+
 });
 
 AWS.config.update({region: 'us-east-1'});
+
