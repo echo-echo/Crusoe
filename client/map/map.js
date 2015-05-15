7897b1c31d039d6e37affe247186e96e910ebe92
Josh Benson <bensonjosha@gmail.com>
2015-05-15 15:30:11 -0500

    refactored tracker autorun and commented heavily

--- client/map/map.js
+++ client/map/map.js
@@ -31,31 +31,30 @@ Template.Map.onRendered(function () {
       //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       // pulls all the messages from the database.
       // re-initializes the users location variables and
-      // initializes some other variables
       //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       var allMess = Messages.find({},{sort: {createdAt: -1}}).fetch();
       var userLat = Number(localStorage.getItem("userLat"));
       var userLong = Number(localStorage.getItem("userLong"));
-      var geoJsons = [];
-      var messIds = messIds ? messIds : {};
-
-
 
       //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       // if the map isnt initialized then initialize it.
       //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       if (!map) {
-        //changes the width of the map
+        //changes the width of the map based on window size
         if ($(window).width() > 480) {
           $('#map').width($(window).width()-300);
+        } else if ($(window).width() <= 480){
+          $('#map').width($(window.width()));
         }
 
+        //grabs mapbox layers from our mapbox account.
         L.mapbox.accessToken = "pk.eyJ1Ijoic2tpbm5lcjUyMCIsImEiOiJHOXRJeUlFIn0.ZVhoykCRgo8-_KQl2-x9MQ";
         map = L.mapbox.map('map', 'skinner520.fbb71f90', {
           attributionControl: false,
           zoomControl :false
         });
 
+        //initializes marker, users location, user radius, and pans map to the user's location
         marker = L.marker([userLat, userLong]).addTo(map);
         map.panTo([userLat, userLong]); //TODO: change this to pan to clicked message location
         map.setZoom(14);
@@ -65,8 +64,8 @@ Template.Map.onRendered(function () {
       }
 
       //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      // Updates the user location and pans to that location if
-      // the Pan variable is set to true
+      // Updates the user location and radius marker
+      // and pans to that location if the Pan variable is set to true
       //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        var local = Geolocation.currentLocation();
         if(local){
@@ -81,13 +80,26 @@ Template.Map.onRendered(function () {
             map.panTo([local.coords.latitude, local.coords.longitude]);
           }
         }
+      //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      /*    SUMMARY OF THE FOLLOWING CODE
+      *   the 'messagesOnMap' variable is an object whos keys are messageId's
+      *   which we have retrieved from the database. These keys represent the message in our databse. 
+      *   The Value of these keys are the leaflet ids, refering to the marker on the map. 
+      *
+      *  The following code checks to see if the messages fetched from the server already have existing 
+      *  leaflet id's on the map. If they DO, then we update their location/icon if neccessary. If they DONT
+      *  we create the appropriate icon and add it to the map.
+      */
+      //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-      //check to see whether a message is already on the map based on its _id:
-      var checkLayers = {};
-      geoJsonLayer.eachLayer(function(layer) {
-        checkLayers[layer.feature.properties.id] = layer._leaflet_id;
+      //this object is used to check whether or not the messages exist on the map
+      var messagesOnMap = {}; 
+      //associates the messageID with the leafletId
+      geoJsonLayer.eachLayer(function(layer) {  
+        messagesOnMap[layer.feature.properties.id] = layer._leaflet_id;
       });
+
+      // // Adds the classname of the icon to the messages as we add them to the map. 
       geoJsonLayer.on('layeradd', function (e) {
         var marker = e.layer,
         feature = marker.feature;
@@ -98,68 +110,102 @@ Template.Map.onRendered(function () {
           iconSize: [35,35]
         }));
       });
-      allMess.forEach(function(object){
-        var geoJsonNew;
-        var msgLat = object.location.coordinates[1];
-        var msgLong = object.location.coordinates[0];
 
+  
+      // for each message that we have gotten from the server (object)
+      allMess.forEach(function(object){
+       
+        // we grab the new coordinates for the message
+        var newMsgLat = object.location.coordinates[1];
+        var newMsgLng = object.location.coordinates[0];
+
+        // we check to see if the message is popular or if it belongs to the user
+        // and set the appropriate variables for assign icons later. 
         var isPopular = object.opens > 5;
         if(Meteor.user()){
           var isUsers = object.username !== "Anonymous" && object.username === Meteor.user().username
         }
-        var proximity = getProx(msgLat,msgLong,userLat,userLong) < radiusVal;
-        var currStat = geoJsonLayer.getLayer( checkLayers[object._id] ) || false
-        currStat = currStat ? currStat.feature.properties.title !== "too far to view message" : currStat;
 
-        if (checkLayers.hasOwnProperty(object._id) && proximity===currStat) {
-          geoJsonLayer.getLayer(checkLayers[object._id]).setLatLng([object.location.coordinates[1], object.location.coordinates[0]])
+        //checks if message already exists on the map
+        //'isOldMessage' is true if the message already has a representative icon on the map
+        var isOldMessage = !!geoJsonLayer.getLayer( messagesOnMap[object._id] ) || false
+
+        //if it is already on the map, we grab the appropriate information and save it to a variable.
+        if(isOldMessage){
+          var oldMessage = geoJsonLayer.getLayer( messagesOnMap[object._id]);
+          var oldMessageInRange = getProx(oldMessage.feature.geometry.coordinates[1], oldMessage.feature.geometry.coordinates[0], userLat, userLong) < radiusVal;
+        }
+
+        // we check to see if the new coordinates of the message are in range of the user
+        var newMessageInRange = getProx(newMsgLat,newMsgLng,userLat,userLong) < radiusVal;
+
+
+        /* 
+        * We now need to check if the state of the icon has changed
+        * if the message was already on the map, and has either entered or 
+        * left the users readable radius, then we need to update the icon. 
+        *
+        * if the icon has not changed states (hasn't left or entered users
+        * readable radius), then we just update the location of the icon
+        */
+
+
+        //old version of message and new version of message have not changed view state ==> doesnt require icon change
+        if(isOldMessage && oldMessageInRange === newMessageInRange){
+          oldMessage.setLatLng([object.location.coordinates[1], object.location.coordinates[0]]).update();
+
+        //the message has changed states, and need an icon change. 
         } else {
-          if (currStat) { geoJsonLayer.removeLayer( checkLayers[object._id] )}
-            // var msgLat = object.location.coordinates[1];
-            // var msgLong = object.location.coordinates[0];
-            var proximity = getProx(msgLat,msgLong,userLat,userLong);
+          //removes the old marker of the message that matches the message id
+          if(isOldMessage){ 
+            geoJsonLayer.removeLayer(messagesOnMap[object._id]) 
+          } 
 
-            geoJsonNew = {
-                  "type": "Feature",
-                  "geometry": {
-                    "type": "Point",
-                    "coordinates": [msgLong, msgLat]
-                  },
-                  "properties": {
-                    "title": object.text,
-                    "id": object._id,
-                    "description": object.createdAt,
-                    "icon": {
-                      "iconUrl": "message-user",
-                      "iconSize": [35, 35]
-                    }
-                  }
-                };
+          //add the new marker with the new message properties
+          var geoJsonNew = {
+                        "type": "Feature",
+                        "geometry": {
+                          "type": "Point",
+                          "coordinates": [newMsgLng, newMsgLat]
+                        },
+                        "properties": {
+                          "title": object.text,
+                          "id": object._id,
+                          "description": object.createdAt,
+                          "icon": {
+                            "iconUrl": "message-user",
+                            "iconSize": [35, 35]
+                          }
+                        }
+                      };
 
-            if(isUsers){
-              geoJsonNew.properties.icon.iconUrl = "message-user"
-            }else if (proximity<radiusVal){
-              if(isPopular){
-                geoJsonNew.properties.icon.iconUrl = "close-pop"
-              } else {
-                geoJsonNew.properties.icon.iconUrl = "close"
-              }
-            }else{
-              if(isPopular){
-                geoJsonNew.properties.icon.iconUrl = "far-pop"
-                geoJsonNew.properties.title = "too far to view message"
-              } else {
-                geoJsonNew.properties.icon.iconUrl = "far"
-                geoJsonNew.properties.title = "too far to view message"
-              }
+          //assign the appropriate message icon/title
+          if(isUsers){
+            geoJsonNew.properties.icon.iconUrl = "message-user"
+          }else if (newMessageInRange){
+            if(isPopular){
+              geoJsonNew.properties.icon.iconUrl = "close-pop"
+            } else {
+              geoJsonNew.properties.icon.iconUrl = "close"
             }
-        geoJsonLayer.addData(geoJsonNew);
+          }else{
+            if(isPopular){
+              geoJsonNew.properties.icon.iconUrl = "far-pop"
+              geoJsonNew.properties.title = "too far to view message"
+            } else {
+              geoJsonNew.properties.icon.iconUrl = "far"
+              geoJsonNew.properties.title = "too far to view message"
+            }
+          }
+
+          //add the new marker to the map.
+          geoJsonLayer.addData(geoJsonNew);
         }
       });
 
       //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       //  listener for when a user clicks on a message in the map
-       //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
       geoJsonLayer.on('click', function (e) {
           Session.set('messageId', e.layer.feature.properties.id)
@@ -190,10 +236,6 @@ Template.Map.helpers({
   }
 })
 
-
-
-
-
 //needed the same method for a different template.
 Template.toofar.helpers({
   message: function(){
@@ -202,9 +244,6 @@ Template.toofar.helpers({
     return message;
   }
 });
-
-
-
 
 //~~~~~~~~ HELPER FUNCTIONS ~~~~~~~~~~~*/
 //find distance btwn two points on sphere
