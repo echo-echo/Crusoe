c877df944d5d0fce9558365a9ab28ce1b05806f9
Ben Johnson <ben@bjmfactory.com>
2015-05-25 14:44:37 -0500

    removes test that checks for window.Crusoe.map

--- tests/mocha/client/sampleClientTest.js
+++ tests/mocha/client/sampleClientTest.js
@@ -4,16 +4,12 @@ var expect = chai.expect
 
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
-
     describe('Crusoe window object', function(){
       it("should exist", function(){
        should.exist(window.Crusoe)
       })
       it("should have a lastCalled property", function(){
        should.exist(window.Crusoe.lastCalled)
-      })
-      it("should have a map property", function(){
-        should.exist(window.Crusoe.map);
       })
       it("should have mapbox", function(){
        should.exist(Mapbox)
